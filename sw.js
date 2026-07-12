652460c928c6bee4d59079fbeb346641a1545ce1
vipt990h-dotcom <vipt990h@gmail.com>
2026-07-12 22:14:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iqra-enterprise-v1-certificates';
+const CACHE='iqra-enterprise-v1-2-certificates';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
